3fc080650298665bb18da99a7d2045b617db8921
pauldps <1726774+pauldps@users.noreply.github.com>
2026-02-07 20:29:51 -0500

    Add Databases struct for managing multiple database pools

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,6 +1,8 @@
+pub mod databases;
 pub mod main_database;
 pub mod session_database;
 pub mod sqlite_database;
 
+pub use databases::Databases;
 pub use main_database::MainDatabase;
 pub use session_database::SessionDatabase;
